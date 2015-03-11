d4b8ce1c3408615aaaeb7ea633b67b161c214415
Vladimír Mach <vladimir@mach.im>
2015-03-11 15:30:40 +0100

    Add viewmodel for OUs.

---
+++ ActiveDirectoryComputerInfoUpdater/ViewModel/OrganizationalUnitsTreeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Mach.Wpf.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryComputerInfoUpdater.ViewModel
+{
+    public class OrganizationalUnitsTreeViewModel : NotifyPropertyBase
+    {
+    }
+}
--- ActiveDirectoryComputerInfoUpdater/ViewModel/WindowViewModel.cs
+++ ActiveDirectoryComputerInfoUpdater/ViewModel/WindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mach.Wpf.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace ActiveDirectoryComputerInfoUpdater.ViewModel
 {
-    public class WindowViewModel
+    public class WindowViewModel : NotifyPropertyBase
     {
+        private OrganizationalUnitsTreeViewModel _organizationalUnits;
+
+        public OrganizationalUnitsTreeViewModel OrganizationalUnits
+        {
+            get { return _organizationalUnits; }
+            set
+            {
+                _organizationalUnits = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public WindowViewModel()
+        {
+
+        }
     }
 }
